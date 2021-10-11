fa849a6b68d4963052dfc36e0548c574626a192f
carolinaortegaoc <caroortega@hotmail.com>
2021-10-10 22:33:34 -0300

    desafio menu clase 3

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Style/navbarSASS.scss'
+
+export const Navbar = () => {
+        return (
+            <>
+                <nav className='nav'>
+                <div className='container_title'>
+                <h1>lavender town</h1>
+                </div>
+                <ul className='navUl'>
+                    <li className='navLi'><a href="#" className="a">Inicio</a></li>
+                    <li className='navLi'><a href="#" className="a">Productos</a></li>
+                    <li className='navLi'><a href="#" className="a">Nosotros</a></li>
+                    <li className='navLi'><a href="#" className="a">Contactanos</a></li>
+                </ul>
+                </nav>
+            </>
+        )
+    }
